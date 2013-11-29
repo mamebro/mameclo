bc4619d195cbab545e1e7f05340c1e1a5b3958f1
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2013-11-29 18:23:09 +0900

    add a entry command

--- lib/mameclo/cli.rb
+++ lib/mameclo/cli.rb
@@ -1,11 +1,17 @@
 require 'mameclo'
 require 'thor'
+require 'faraday'
+require 'json'
 
 module Mameclo
   class CLI < Thor
-    desc "sing [word]", "まめまめまめまめまめぶろ♪"
-    def sing(word)
-      say(word, :red)
+    desc "entry [number]", "指定した番号のエントリーを表示します"
+    def entry(number)
+      conn = Faraday.new(:url => "http://www.mameblo.com/entries/#{number}.json")
+      response = conn.get
+      body = JSON.parse(response.body)
+      say("title:\n" + body['title'] + "\n\n")
+      say("content:\n" + body['content'])
     end
   end
 end
